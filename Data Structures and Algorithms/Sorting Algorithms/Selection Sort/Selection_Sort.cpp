f749bf713c52da74aa3a66f407872cb29fe66949
selvaraj-kuppusamy <velmanikpvs@gmail.com>
2021-07-30 13:58:17 +0530

    Data Structures and Algorithms

---
+++ Data Structures and Algorithms/Sorting Algorithms/Selection Sort/Selection_Sort.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+void swapping(int &a,int &b)
+{
+	int temp;
+	temp=a;
+	a=b;
+	b=temp;
+}
+void display(int *array,int size)
+{
+	for(int i=0;i<size;i++)
+	{
+		cout<<array[i]<<" ";
+	}
+	cout<<endl;
+}
+void SelectionSort(int *array,int size)
+{
+	int i,j,imin;
+	for(i=0;i<size-1;i++)
+	{
+		imin=i;
+		for(j=i+1;j<size;j++)
+			if(array[j]<array[imin])
+			{
+			imin=j;
+			}
+			swap(array[i],array[imin]);
+	}
+
+}
+int main()
+{
+	int n;
+	cin >>n;
+	int arr[n];
+	for(int i=0;i<n;i++)
+	{
+		cin >> arr[i];
+	}
+	SelectionSort(arr,n);
+	display(arr,n);
+}
